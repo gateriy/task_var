d8262bff86cc0717167256c853c3e67584bb5328
Nick Fedoseev <gateriy@yandex.ru>
2024-11-14 21:30:51 +0300

    Task 1-2 (Catch2) inset empty-test 14.11.24

--- Task/Task_Catch2/Task_Catch2.cpp
+++ Task/Task_Catch2/Task_Catch2.cpp
@@ -1,102 +1,6 @@
 п»ї//РїРѕРґРєР»СЋС‡Р°РµРј РјР°РєСЂРѕСЃС‹ catch2
-#include "catch_amalgamated.hpp"
-#include <cstdint>
-#include <iostream>
 
-
-struct ListNode
-{
-public:
-    ListNode(int value, ListNode* prev = nullptr, ListNode* next = nullptr)
-        : value(value), prev(prev), next(next)
-    {
-        if (prev != nullptr) prev->next = this;
-        if (next != nullptr) next->prev = this;
-    }
-
-public:
-    int value;
-    ListNode* prev;
-    ListNode* next;
-};
-
-
-class List
-{
-public:
-    List()
-        : m_head(new ListNode(static_cast<int>(0))), m_size(0),
-        m_tail(new ListNode(0, m_head))
-    {
-    }
-
-    virtual ~List()
-    {
-        Clear();
-        delete m_head;
-        delete m_tail;
-    }
-
-    bool Empty() { return m_size == 0; }
-
-    unsigned long Size() { return m_size; }
-
-    void PushFront(int value)
-    {
-        new ListNode(value, m_head, m_head->next);
-        ++m_size;
-    }
-
-    void PushBack(int value)
-    {
-        new ListNode(value, m_tail->prev, m_tail);
-        ++m_size;
-    }
-
-    int PopFront()
-    {
-        if (Empty()) throw std::runtime_error("list is empty");
-        auto node = extractPrev(m_head->next->next);
-        int ret = node->value;
-        delete node;
-        return ret;
-    }
-
-    int PopBack()
-    {
-        if (Empty()) throw std::runtime_error("list is empty");
-        auto node = extractPrev(m_tail);
-        int ret = node->value;
-        delete node;
-        return ret;
-    }
-
-    void Clear()
-    {
-        auto current = m_head->next;
-        while (current != m_tail)
-        {
-            current = current->next;
-            delete extractPrev(current);
-        }
-    }
-
-private:
-    ListNode* extractPrev(ListNode* node)
-    {
-        auto target = node->prev;
-        target->prev->next = target->next;
-        target->next->prev = target->prev;
-        --m_size;
-        return target;
-    }
-
-private:
-    ListNode* m_head;
-    ListNode* m_tail;
-    unsigned long m_size;
-};
-
+#include "my_list.h"
 
 //--------------РїСЂРѕРІРµСЂРєРё------------
 
@@ -220,6 +124,14 @@ TEST_CASE("PopBack test", "[popback]") {
     CHECK(class_test.Size() == 0);
     class_test.Clear();
     std::cout << "------------------------------------------" << std::endl;
+
+    //РІРѕ РІСЂРµРјСЏ РІС‹С‡РёСЃР»РµРЅРёСЏ РІС‹СЂР°Р¶РµРЅРёСЏ Р±СѓРґРµС‚ СЃРіРµРЅРµСЂРёСЂРѕРІР°РЅРѕ РёСЃРєР»СЋС‡РµРЅРёРµ (Р»СЋР±РѕРіРѕ С‚РёРїР°).
+    REQUIRE_THROWS(class_test.PopBack());
+    //Р±СѓРґРµС‚ СЃРіРµРЅРµСЂРёСЂРѕРІР°РЅРѕ РёСЃРєР»СЋС‡РµРЅРёРµ, РєРѕС‚РѕСЂРѕРµ РїСЂРё РїСЂРµРѕР±СЂР°Р·РѕРІР°РЅРёРё РІ СЃС‚СЂРѕРєСѓ Р±СѓРґРµС‚ 
+    // СЃРѕРѕС‚РІРµС‚СЃС‚РІРѕРІР°С‚СЊ РїСЂРµРґРѕСЃС‚Р°РІР»РµРЅРЅРѕР№ СЃС‚СЂРѕРєРµ (СЃРїРµС†РёР°Р»СЊРЅРѕ РїРѕСЃС‚Р°РІРёР» РѕС‚Р»РёС‡РЅСѓСЋ СЃС‚СЂРѕРєСѓ)
+    CHECK_THROWS_WITH(class_test.PopBack(), "list is not empty");
+    //РћР¶РёРґР°РµС‚СЃСЏ, С‡С‚Рѕ РІРѕ РІСЂРµРјСЏ РІС‹С‡РёСЃР»РµРЅРёСЏ РІС‹СЂР°Р¶РµРЅРёСЏ РЅРµ Р±СѓРґРµС‚ РІС‹РґР°РЅРѕ РЅРёРєР°РєРёС… РёСЃРєР»СЋС‡РµРЅРёР№.
+    REQUIRE_NOTHROW(class_test.PopBack());
 }
 
 //PopFront
@@ -250,4 +162,13 @@ TEST_CASE("PopFront test", "[PopFront]") {
 
   
     std::cout << "------------------------------------------" << std::endl;
+
+    //РІРѕ РІСЂРµРјСЏ РІС‹С‡РёСЃР»РµРЅРёСЏ РІС‹СЂР°Р¶РµРЅРёСЏ Р±СѓРґРµС‚ СЃРіРµРЅРµСЂРёСЂРѕРІР°РЅРѕ РёСЃРєР»СЋС‡РµРЅРёРµ (Р»СЋР±РѕРіРѕ С‚РёРїР°).
+    REQUIRE_THROWS(class_test.PopFront());
+    //Р±СѓРґРµС‚ СЃРіРµРЅРµСЂРёСЂРѕРІР°РЅРѕ РёСЃРєР»СЋС‡РµРЅРёРµ, РєРѕС‚РѕСЂРѕРµ РїСЂРё РїСЂРµРѕР±СЂР°Р·РѕРІР°РЅРёРё РІ СЃС‚СЂРѕРєСѓ Р±СѓРґРµС‚ 
+    // СЃРѕРѕС‚РІРµС‚СЃС‚РІРѕРІР°С‚СЊ РїСЂРµРґРѕСЃС‚Р°РІР»РµРЅРЅРѕР№ СЃС‚СЂРѕРєРµ (СЃРїРµС†РёР°Р»СЊРЅРѕ РїРѕСЃС‚Р°РІРёР» РѕС‚Р»РёС‡РЅСѓСЋ СЃС‚СЂРѕРєСѓ)
+    CHECK_THROWS_WITH(class_test.PopFront(), "list is not empty");
+    //РћР¶РёРґР°РµС‚СЃСЏ, С‡С‚Рѕ РІРѕ РІСЂРµРјСЏ РІС‹С‡РёСЃР»РµРЅРёСЏ РІС‹СЂР°Р¶РµРЅРёСЏ РЅРµ Р±СѓРґРµС‚ РІС‹РґР°РЅРѕ РЅРёРєР°РєРёС… РёСЃРєР»СЋС‡РµРЅРёР№.
+    REQUIRE_NOTHROW(class_test.PopFront());
+
 }
