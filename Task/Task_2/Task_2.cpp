b9bfd05cce3e24137dcb967376f14a641fd801f0
Nick Fedoseev <gateriy@yandex.ru>
2025-07-09 20:10:39 +0300

    Task_1-2 Thread 09.07.25

--- Task/Task_2/Task_2.cpp
+++ Task/Task_2/Task_2.cpp
@@ -9,6 +9,8 @@
 #include <functional>
 #include <exception>
 
+
+
 /*
 Задание 2
 Параллельные вычисления
@@ -22,11 +24,18 @@
 
 std::mutex mt;
 
+void SetCursor(int x, int y) {
+
+    COORD position = { x,y }; //позиция x и y
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hConsole, position);
+}
+
 struct AAA {
 
     AAA() {}
 
-    AAA(const std::vector<long long int>& vect_a, const std::vector<long long int>& vect_b, const int blocks) :
+    AAA(const std::vector<long long int>& vect_a, const std::vector<long long int>& vect_b, const int& blocks) :
         _blocks(blocks), _vect_a(vect_a), _vect_b(vect_b)
     {
         _summ_var.clear();
@@ -35,7 +44,7 @@ struct AAA {
 
         //создание вектора лямбд - отдельно чтобы иметь гибкость в системе
         for (int i = 0; i < _blocks; ++i) {
-            _vec_lambda.push_back(([this, i]() {summ_block((i + 1));}));
+            _vec_lambda.push_back(([this, i]() {summ_block((i + 1)); }));
         }
 
         //создание вектора потоков с одновременной инициализацией в потоках функций из векктора лямбд
@@ -52,7 +61,7 @@ struct AAA {
         for (const auto& f : _summ_var) {
             _summ_x = _summ_x + f;
         }
-        std::cout << "Итого потоков: " << _blocks << " сумма: " << _summ_x << std::endl;
+        //std::cout << "Итого потоков: " << _blocks << " сумма: " << _summ_x << std::endl;
     }
 
     ~AAA() {};
@@ -81,10 +90,10 @@ struct AAA {
         else { std::cout << "Ошибка - исходные вектора разной длины!"; }
 
         //установка и снятие флага на блокировку доступа к ощему вектору промежуточных сумм класса с его наполнением
-          
-        mt.lock(); 
+
+        mt.lock();
         _summ_var.push_back(summ);
-        mt.unlock();        
+        mt.unlock();
     }
 
     //гененратор вектора
@@ -121,7 +130,10 @@ int main()
     std::string text{ "y" };
     bool go_x{ false };
 
-    int size_x{ 1000003 }, cof_1{ 1 }, cof_2{ 1 };
+    std::vector <int> block_x{ 1, 2, 4, 8, 16 };//блоки нарезки векторов
+    std::vector <int> size_x{ 1000, 10000, 100000, 1000000 };//размеры векторов
+    int  cof_1{ 1 }, cof_2{ 1 };//множители значений элементов векторов
+    std::vector<std::pair<long long int, std::pair<int, std::pair<int, double>>>> res_x; //вектор результатов (сумма,(размер вектора,(блоков, время)))
 
     std::cout << "Пример потоков\n";
     std::cout << std::endl;
@@ -133,45 +145,72 @@ int main()
         AAA a_1_1;
         AAA a_1_2;
 
-        std::cout << "Введите размер векторв (не более 10000000): ";
-        std::cin >> size_x;
         try {
+            res_x.clear();
+            for (const auto& s : size_x) {
 
-            //генерация вектора а
-            std::thread x_gen_1([&]() {
-                a_1_1._vector_x(size_x, cof_1);
-                std::cout << "Поток 1 - создание вектора." << std::endl;
-                });     //std::thread x_1(&AAA::vector_x, &a1, 100); //аналогично!
-            //генерация вектора в
-            std::thread x_gen_2([&]() {
-                a_1_2._vector_x(size_x, cof_2);
-                std::cout << "Поток 2 - создание вектора." << std::endl;
-                });
-            x_gen_2.join();
-            x_gen_1.join();
+                //генерация вектора а
+                std::thread x_gen_1([&]() {
+                    a_1_1._vector_x(s, cof_1);
+                    });     //std::thread x_1(&AAA::vector_x, &a1, 100); //аналогично!
+                //генерация вектора в
+                std::thread x_gen_2([&]() {
+                    a_1_2._vector_x(s, cof_2);
+                    });
+                x_gen_2.join();
+                x_gen_1.join();
 
+                if ((x_gen_1.joinable() == false) && (x_gen_2.joinable() == false)) {
 
+                    for (const auto& i : block_x) {
 
+                        auto start = std::chrono::steady_clock::now();
 
-        std::cout << std::endl;
+                        AAA a1(a_1_1._vect_x, a_1_2._vect_x, i);
 
-        if ((x_gen_1.joinable() == false) && (x_gen_2.joinable() == false)) {
+                        auto end = std::chrono::steady_clock::now();
+                        std::chrono::duration<double> elapsed_seconds = end - start;
 
-            for (int i = 1; i <= 16; ++i) {
+                        res_x.push_back({ a1._summ_x,{ s,{i, elapsed_seconds.count() } } });
 
-                auto start = std::chrono::steady_clock::now();
-
-                AAA a1(a_1_1._vect_x, a_1_2._vect_x, i);
-
-                auto end = std::chrono::steady_clock::now();
-                std::chrono::duration<double> elapsed_seconds = end - start;
-                std::cout << "Время выполнения: " << elapsed_seconds.count() << "s\n";
-                std::cout << std::endl;
-
-                a1.~AAA();
+                        a1.~AAA();
+                    }
+                };
             }
-        };
 
+            int count{ 0 };
+
+            std::cout << "Размер вектора:";
+            for (const auto& text_1 : size_x) {
+                SetCursor( (count*20+20),4);
+                std::cout << text_1;
+                ++count;
+            }
+            std::cout << std::endl;
+            for (int i = 0; i < 50; ++i) {
+                std::cout << "--";
+            }            
+            for (int f = 0; f < 5; f++) {
+                SetCursor(0,f+6);
+                std::cout << "Потоков:";
+                SetCursor(10, f + 6);
+                std::cout << res_x.at(f).second.second.first;
+            }
+            count = 0;
+            for (int f = 0; f < 4; f++) {
+                for (int i = 0; i < 5; ++i) {
+                    SetCursor(f * 20 + 20, i + 6);
+                    //std::cout << "S: " << res_x.at(count).first;
+                    std::cout << res_x.at(count).second.second.second << " (s)";
+                    ++count;
+                }
+            }            
+
+            std::cout << std::endl;
+            for (int i = 0; i < 50; ++i) {
+                std::cout << "--";
+            }
+            std::cout << std::endl;
         }
         catch (std::exception& e)
         {
@@ -181,10 +220,10 @@ int main()
         {
             std::cerr << "Не известное исключение!" << std::endl;
         }
-      
+
         std::cout << "Продолжить? Веедите y/n: ";
         std::cin >> text;
-        
+
         if (text == "y") {
             go_x = true;
 
